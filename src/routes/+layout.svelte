1611418bea0caa228d5bfda060c3e9820ba5c7fe
tpi7 <thibault.parodi@gmail.com>
2026-01-14 17:10:59 +0100

    style: add basic css and font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,3 +4,12 @@
 </script>
 
 {@render children()}
+
+<!-- sveltehead -->
+<svelte:head>
+    <title>Chakas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&display=swap" rel="stylesheet">
+</svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="container mx-auto p-4">
+    <h1 class="text-4xl font-bold">Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</div>
